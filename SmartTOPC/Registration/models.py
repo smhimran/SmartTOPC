00d3ed418771552a7d26277df1a65c8be6f6cc1e
Yankee-16 <46569507+Yankee-16@users.noreply.github.com>
2020-02-19 02:23:07 +0600

    Add files via upload

---
+++ SmartTOPC/Registration/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    dept = (
+        ('Cse', "CSE"),
+        ('Soft', 'SWE'),
+        ('cis', 'CIS'),
+        ('other', 'Other'),
+    )
+    camp = (
+        ('Main', 'MC'),
+        ('Uttara', 'UC'),
+    )
+    sem = (
+        ('first', '1st'),
+        ('second', '2nd'),
+    )
+    shft = (
+        ('day', 'Day'),
+        ('eve', 'Evening'),
+    )
+    sec = (
+        ('a', 'A'),
+        ('b', 'B'),
+        ('c', 'C'),
+        ('d', 'D'),
+        ('e', 'E'),
+    )
+    tshirt = (
+        ('mid', 'M'),
+        ('large', 'L'),
+        ('xlarge', 'XL'),
+        ('xxlarge', 'XXL'),
+        ('xxxlarge', 'XXXL'),
+    )
+
+    ID = models.CharField(max_length=20, unique=True, primary_key=True)
+    Name = models.CharField(max_length=50)
+    Department = models.CharField(max_length=10, choices=dept, default='Cse')
+    Campus = models.CharField(max_length=10, choices=camp, default='Main')
+    Semester = models.CharField(max_length=10, choices=sem, default='first')
+    Shift = models.CharField(max_length=10, choices=shft, default='day')
+    Section = models.CharField(max_length=10, choices=sec)
+    TShirt = models.CharField(max_length=10, choices=tshirt, default='mid')
+    Status = models.BooleanField(default=False)
+    # Token = models.AutoField(auto_created=True, unique=True, default=1001, serialize=True)
+
+    def __str__(self):
+        return self.Name
